7ef98c8940474e36e80f00ebed9a6f9dc9dee967
berkaypacal <pacalberkay@gmail.com>
2022-12-24 17:48:50 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
 import { authentication } from "../service/firebaseConfig";
 import React, { Component } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const [mail, setMail] = React.useState("");
+  const router = useRouter();
 
   function signInFb() {
     const provider = new FacebookAuthProvider();
+    const { redirect } = router.query;
     signInWithPopup(authentication, provider)
       .then((res) => {
         setMail(res.user.email);
-        window.location.href = "payt://?code=" + res.user.email;
+        window.location.href = "" + redirect + "://?code=" + res.user.email;
         console.log(res);
       })
       .catch((err) => {
