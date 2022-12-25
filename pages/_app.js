fdef5020b5ad903adf5f8c36966b7e2937174a2d
berkaypacal <pacalberkay@gmail.com>
2022-12-25 15:24:59 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import { getRedirectResult, onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { authentication } from "../service/firebaseConfig";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import { SessionProvider } from "next-auth/react";
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,14 @@ import {
   signInWithPopup,
   FacebookAuthProvider,
   onAuthStateChanged,
-  signOut,
 } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { authentication } from "../service/firebaseConfig";
 
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
   const [mail, setMail] = useState("");
-
-  useEffect(() => {
-    signOut(authentication);
-  }, []);
-
   function signInFb() {
     const provider = new FacebookAuthProvider();
 
@@ -38,25 +34,34 @@ export default function Home() {
       });
   }
 
-  onAuthStateChanged(authentication, (user) => {
-    if (user != null && user != undefined) {
-      console.log(user);
-      window.location.href = "vfb://?code=" + user.email;
-    }
+  // onAuthStateChanged(authentication, (user) => {
+  //   if (user != null && user != undefined) {
+  //     console.log(user);
+  //     window.location.href = "vfb://?code=" + user.email;
+  //   }
 
-    //window.close();
-  });
+  //   //window.close();
+  // });
 
+  function nextauth() {
+    signIn();
+  }
+
+  const { data: session } = useSession();
+
+  if (session) {
+    console.log("giriş yapıldu" + session.user.email);
+    return (
+      <>
+        Signed in as {session.accessToken} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
     <>
-      <button
-        onClick={() => {
-          signInFb();
-        }}
-      >
-        Test
-      </button>
-      <p>{mail}</p>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
     </>
   );
 }
