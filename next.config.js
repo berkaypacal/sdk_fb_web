0a3d5025ba864bd226351b82194f398ece47df63
berkaypacal <pacalberkay@gmail.com>
2022-12-25 16:31:21 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/__/auth/:path*",
+        destination: `https://sdkverocity.firebaseapp.com/__/auth/:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
