2c5e7e74c1b836315d05700f33e8d4dae1237eb6
berkaypacal <pacalberkay@gmail.com>
2022-12-26 10:13:48 +0300

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,32 +10,8 @@ export const authOptions = {
       clientSecret: process.env.NEXT_PUBLIC_SECRET,
       idToken: true,
       authorization: {
-        url: "https://www.facebook.com/v15.0/dialog/oauth",
         params: {
-          client_id: process.env.NEXT_PUBLIC_ClIENT_ID,
-          scope: "openid email",
-          response_type: "code",
-        },
-      },
-      token: {
-        url: "https://graph.facebook.com/oauth/access_token",
-        async request(context) {
-          try {
-            // request to https://graph.facebook.com/oauth/access_token?code=""&client_id=""&redirect_uri=""&client_secret=""
-            const response = await axios.get(this.url, {
-              params: {
-                code: context.params.code,
-                client_id: context.provider.clientId,
-                redirect_uri: context.provider.callbackUrl,
-                client_secret: context.provider.clientSecret,
-              },
-            });
-
-            const tokens = response.data;
-            return { tokens };
-          } catch (error) {
-            throw error;
-          }
+          scope: "email",
         },
       },
     }),
