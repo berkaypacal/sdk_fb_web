6c91606aebef6d0e0e683d0d07a865ad67e7e9ad
berkaypacal <pacalberkay@gmail.com>
2023-01-01 19:28:31 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  FacebookAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { authentication } from "../service/firebaseConfig";
-
 export default function Home() {
   const [mail, setMail] = useState("");
   function signInFb() {
     const provider = new FacebookAuthProvider();
-
     signInWithPopup(authentication, provider)
       .then((res) => {
         setMail("thene girdi");
@@ -15,14 +17,11 @@ export default function Home() {
           setMail("Başarılı");
 
           console.log(res.user);
-          window.location.href = "vfb://?code=" + res.user.displayName;
+          window.location.href = "payt://?code=" + res.user.displayName;
         }
       })
       .catch((err) => {
         setMail("HATA catch " + err);
-        console.error(err);
-        console.warn(err);
-        console.log(err);
       });
   }
 
