fb376685d2a62184be7ca3f83a79574674e92b7c
berkaypacal <pacalberkay@gmail.com>
2023-01-06 12:57:32 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,3 @@
-import AppleAssociation from "./AppleAssociation";
-import { Container as NextContainer } from "next/app";
-import Head from "next/head";
-
 export default function App({ Component, pageProps }) {
-  return (
-    <Component {...pageProps}>
-      <Head>
-        <AppleAssociation />
-      </Head>
-    </Component>
-  );
+  return <Component {...pageProps} />;
 }
