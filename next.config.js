78f6fbc71837b8dcc131a826188f11bcc07aa2c8
berkaypacal <pacalberkay@gmail.com>
2023-01-06 13:16:24 +0300

    update

--- next.config.js
+++ next.config.js
@@ -7,6 +7,24 @@ module.exports = {
         source: "/__/auth/:path*",
         destination: `https://sdkverocity.firebaseapp.com/__/auth/:path*`,
       },
+      {
+        source: "/apple-app-site-association",
+        destination: "/public/apple-app-site-association",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/apple-app-site-association",
+        locale: false,
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
     ];
   },
 };
