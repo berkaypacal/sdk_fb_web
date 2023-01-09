1be19aefc955f768bf8421c51dbbbc52dee741f2
fatihturan <fatih.turan@mtdt.com>
2023-01-09 22:16:44 +0300

    2

--- pages/test.js
+++ pages/test.js
@@ -5,7 +5,7 @@
       <>
       <a href="https://sdk-fb-web-iszq.vercel.app" >TIKLA</a>
         <button onClick={() => {
-           window.location.href = "https://sdk-fb-web-iszq.vercel.app"
+           window.open = "https://sdk-fb-web-iszq.vercel.app"
         }}>Sign in Facebook</button>
       </>
     );
