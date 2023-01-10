7de10f3d4d747e270850b9fb6e3513c906c65738
berkaypacal <pacalberkay@gmail.com>
2023-01-10 19:52:44 +0300

    update

--- next.config.js
+++ next.config.js
@@ -13,4 +13,17 @@ module.exports = {
       // },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/apple-app-site-association",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/json",
+          },
+        ],
+      },
+    ];
+  },
 };
